fb812d893c8ba45a116822b8b78a8c4eec773fdc
Raleigh Slack <37353502+cyrilscott@users.noreply.github.com>
2023-07-23 22:40:15 -0400

    Changes

--- vision/worbotsVision.py
+++ vision/worbotsVision.py
@@ -40,30 +40,37 @@ class WorbotsVision:
             ret, frame = self.cap.read()
             frameCopy = frame.copy()
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            
+            #Define the board
             dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_250)
             board = cv2.aruco.CharucoBoard((11,8), 0.024, 0.019, dictionary)
-            (corners, ids, rejected) = cv2.aruco.detectMarkers(gray, dictionary)
-            cv2.aruco.drawDetectedMarkers(frameCopy, corners, ids)
+
+            #Detect corners as well as markers
             charucoParams = cv2.aruco.CharucoParameters()
             detectorParams = cv2.aruco.DetectorParameters()
             detector = cv2.aruco.CharucoDetector(board=board, charucoParams=charucoParams, detectorParams=detectorParams)
-            (charucoCorners, charucoIds, markerCorns, markerIds) = detector.detectBoard(gray)
-            if (np.size(self.allCharucoCorners) ==0):
+            (charucoCorners, charucoIds, markerCorners, markerIds) = detector.detectBoard(gray)
+
+            #Draw the corners and markers
+            cv2.aruco.drawDetectedCornersCharuco(frameCopy, charucoCorners, charucoIds, (0, 0, 255))
+            cv2.aruco.drawDetectedMarkers(frameCopy, markerCorners, markerIds, (0, 255, 0))
+
+            if (np.size(self.allCharucoCorners) == 0):
                 self.allCharucoCorners = charucoCorners
                 self.allCharucoIds = charucoIds 
-            else:
-                self.allCharucoCorners = np.append(self.allCharucoCorners, charucoCorners, axis=0)
-                self.allCharucoIds = np.append(self.allCharucoIds, charucoIds, axis=0)
-            cv2.aruco.drawDetectedCornersCharuco(frameCopy, charucoCorners, charucoIds, (0, 0, 255))
+            # else:
+            #     self.allCharucoCorners = np.append(self.allCharucoCorners, charucoCorners, 0)
+            #     self.allCharucoIds = np.append(self.allCharucoIds, charucoIds, 0)
+            
             # while (self.allImagePoints):
             #     if(charucoCorners is not None):
             #         (objPoints, imgPoints) = board.matchImagePoints(charucoCorners, charucoIds)
 
             # if (objPoints is not None and imgPoints is not None):
             #     ret, self.mtx, self.dist, rvecs, tvecs = cv2.calibrateCamera([objPoints], [imgPoints], gray.shape[::-1], self.mtx, self.dist)
-            if(np.size(self.allCharucoIds) > 100):
-                ret, self.mtx, self.dist, rvecs, tvecs = cv2.aruco.calibrateCameraCharuco(self.allCharucoCorners, self.allCharucoIds, board, gray.shape[::1], self.mtx, self.dist)
-                print(self.mtx)
+            # if(np.size(self.allCharucoIds) >200):
+            #     ret, self.mtx, self.dist, rvecs, tvecs = cv2.aruco.calibrateCameraCharuco(self.allCharucoCorners, self.allCharucoIds, board, gray.shape[::1], self.mtx, self.dist)
+            #     print(self.mtx)
 
             cv2.imshow("out",frameCopy)
             if (cv2.waitKey(1) & 0xFF == ord('q')):
